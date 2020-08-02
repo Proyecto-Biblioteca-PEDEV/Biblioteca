b238211d14f777d6468723e7bf9ad2cf1b074c72
Sandra <Sandra@DESKTOP-I773KQU>
2020-08-02 03:36:00 -0600

    Avance login

--- Clase III Parcial/Clase III Parcial/Proyecto/Login.Designer.vb
+++ Clase III Parcial/Clase III Parcial/Proyecto/Login.Designer.vb
@@ -23,26 +23,22 @@ Partial Class Login
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login))
         Me.PanelSuperior = New System.Windows.Forms.Panel()
         Me.btnMinimizar = New System.Windows.Forms.Button()
         Me.btnCerrar = New System.Windows.Forms.Button()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.txtContraseña = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.btnIniciar = New System.Windows.Forms.Button()
+        Me.btnMostrarContra = New System.Windows.Forms.Button()
+        Me.txtContrasena = New System.Windows.Forms.TextBox()
+        Me.lblOlvideContra = New System.Windows.Forms.LinkLabel()
         Me.txtUsuario = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.btnIngresar = New System.Windows.Forms.Button()
-        Me.btnCancelar = New System.Windows.Forms.Button()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.ToolTip = New System.Windows.Forms.ToolTip(Me.components)
-        Me.ErrorValidacion = New System.Windows.Forms.ErrorProvider(Me.components)
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnOcultarContra = New System.Windows.Forms.Button()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         Me.PanelSuperior.SuspendLayout()
-        Me.Panel2.SuspendLayout()
-        Me.Panel1.SuspendLayout()
-        CType(Me.ErrorValidacion, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelSuperior
@@ -54,8 +50,8 @@ Partial Class Login
         Me.PanelSuperior.Location = New System.Drawing.Point(0, 0)
         Me.PanelSuperior.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.PanelSuperior.Name = "PanelSuperior"
-        Me.PanelSuperior.Size = New System.Drawing.Size(493, 41)
-        Me.PanelSuperior.TabIndex = 3
+        Me.PanelSuperior.Size = New System.Drawing.Size(505, 41)
+        Me.PanelSuperior.TabIndex = 26
         '
         'btnMinimizar
         '
@@ -66,7 +62,7 @@ Partial Class Login
         Me.btnMinimizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnMinimizar.Image = Global.Clase_III_Parcial.My.Resources.Resources.Icono_Minimizar
-        Me.btnMinimizar.Location = New System.Drawing.Point(405, -11)
+        Me.btnMinimizar.Location = New System.Drawing.Point(417, -11)
         Me.btnMinimizar.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btnMinimizar.Name = "btnMinimizar"
         Me.btnMinimizar.Size = New System.Drawing.Size(40, 64)
@@ -82,154 +78,112 @@ Partial Class Login
         Me.btnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red
         Me.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnCerrar.Image = Global.Clase_III_Parcial.My.Resources.Resources.Icono_cerrar_FN
-        Me.btnCerrar.Location = New System.Drawing.Point(453, -11)
+        Me.btnCerrar.Location = New System.Drawing.Point(465, -11)
         Me.btnCerrar.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btnCerrar.Name = "btnCerrar"
         Me.btnCerrar.Size = New System.Drawing.Size(40, 64)
         Me.btnCerrar.TabIndex = 4
         Me.btnCerrar.UseVisualStyleBackColor = True
         '
-        'Panel2
+        'PictureBox2
         '
-        Me.Panel2.BackColor = System.Drawing.Color.DodgerBlue
-        Me.Panel2.Controls.Add(Me.txtContraseña)
-        Me.Panel2.Controls.Add(Me.Label2)
-        Me.Panel2.Location = New System.Drawing.Point(44, 132)
-        Me.Panel2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(385, 45)
-        Me.Panel2.TabIndex = 9
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.Image = Global.Clase_III_Parcial.My.Resources.Resources.logo_negro_png_super_ultra
+        Me.PictureBox2.Location = New System.Drawing.Point(207, 49)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(78, 72)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 33
+        Me.PictureBox2.TabStop = False
         '
-        'txtContraseña
+        'btnIniciar
         '
-        Me.txtContraseña.Location = New System.Drawing.Point(100, 7)
-        Me.txtContraseña.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.txtContraseña.Multiline = True
-        Me.txtContraseña.Name = "txtContraseña"
-        Me.txtContraseña.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.txtContraseña.Size = New System.Drawing.Size(267, 26)
-        Me.txtContraseña.TabIndex = 1
+        Me.btnIniciar.Location = New System.Drawing.Point(189, 240)
+        Me.btnIniciar.Name = "btnIniciar"
+        Me.btnIniciar.Size = New System.Drawing.Size(96, 31)
+        Me.btnIniciar.TabIndex = 32
+        Me.btnIniciar.Text = "Iniciar Sesion"
+        Me.btnIniciar.UseVisualStyleBackColor = True
         '
-        'Label2
+        'btnMostrarContra
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.ForeColor = System.Drawing.SystemColors.Desktop
-        Me.Label2.Location = New System.Drawing.Point(4, 11)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(88, 25)
-        Me.Label2.TabIndex = 0
-        Me.Label2.Text = "Contraseña"
+        Me.btnMostrarContra.BackColor = System.Drawing.Color.DimGray
+        Me.btnMostrarContra.FlatAppearance.BorderSize = 0
+        Me.btnMostrarContra.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMostrarContra.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.btnMostrarContra.Image = CType(resources.GetObject("btnMostrarContra.Image"), System.Drawing.Image)
+        Me.btnMostrarContra.Location = New System.Drawing.Point(373, 201)
+        Me.btnMostrarContra.Name = "btnMostrarContra"
+        Me.btnMostrarContra.Size = New System.Drawing.Size(43, 20)
+        Me.btnMostrarContra.TabIndex = 29
+        Me.btnMostrarContra.UseVisualStyleBackColor = False
         '
-        'Panel1
+        'txtContrasena
         '
-        Me.Panel1.BackColor = System.Drawing.Color.DodgerBlue
-        Me.Panel1.Controls.Add(Me.txtUsuario)
-        Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(44, 77)
-        Me.Panel1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(385, 45)
-        Me.Panel1.TabIndex = 6
+        Me.txtContrasena.Location = New System.Drawing.Point(92, 196)
+        Me.txtContrasena.Name = "txtContrasena"
+        Me.txtContrasena.Size = New System.Drawing.Size(275, 29)
+        Me.txtContrasena.TabIndex = 28
+        '
+        'lblOlvideContra
+        '
+        Me.lblOlvideContra.AutoSize = True
+        Me.lblOlvideContra.BackColor = System.Drawing.Color.Transparent
+        Me.lblOlvideContra.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOlvideContra.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.lblOlvideContra.LinkColor = System.Drawing.Color.Black
+        Me.lblOlvideContra.Location = New System.Drawing.Point(174, 294)
+        Me.lblOlvideContra.Name = "lblOlvideContra"
+        Me.lblOlvideContra.Size = New System.Drawing.Size(134, 16)
+        Me.lblOlvideContra.TabIndex = 31
+        Me.lblOlvideContra.TabStop = True
+        Me.lblOlvideContra.Text = "Olvide mi contraseña"
         '
         'txtUsuario
         '
-        Me.txtUsuario.Location = New System.Drawing.Point(100, 10)
-        Me.txtUsuario.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.txtUsuario.Multiline = True
+        Me.txtUsuario.Location = New System.Drawing.Point(92, 150)
         Me.txtUsuario.Name = "txtUsuario"
-        Me.txtUsuario.Size = New System.Drawing.Size(267, 26)
-        Me.txtUsuario.TabIndex = 1
+        Me.txtUsuario.Size = New System.Drawing.Size(275, 29)
+        Me.txtUsuario.TabIndex = 27
         '
-        'Label1
+        'btnOcultarContra
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.ForeColor = System.Drawing.SystemColors.Desktop
-        Me.Label1.Location = New System.Drawing.Point(4, 12)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(63, 25)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Usuario"
+        Me.btnOcultarContra.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.btnOcultarContra.FlatAppearance.BorderSize = 0
+        Me.btnOcultarContra.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnOcultarContra.Image = CType(resources.GetObject("btnOcultarContra.Image"), System.Drawing.Image)
+        Me.btnOcultarContra.Location = New System.Drawing.Point(373, 201)
+        Me.btnOcultarContra.Name = "btnOcultarContra"
+        Me.btnOcultarContra.Size = New System.Drawing.Size(29, 24)
+        Me.btnOcultarContra.TabIndex = 30
+        Me.btnOcultarContra.UseVisualStyleBackColor = False
         '
-        'btnIngresar
+        'ErrorProvider1
         '
-        Me.btnIngresar.BackColor = System.Drawing.Color.DeepSkyBlue
-        Me.btnIngresar.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.btnIngresar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CadetBlue
-        Me.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnIngresar.Location = New System.Drawing.Point(44, 187)
-        Me.btnIngresar.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.btnIngresar.Name = "btnIngresar"
-        Me.btnIngresar.Size = New System.Drawing.Size(92, 32)
-        Me.btnIngresar.TabIndex = 8
-        Me.btnIngresar.Text = "Ingresar"
-        Me.btnIngresar.UseVisualStyleBackColor = False
-        '
-        'btnCancelar
-        '
-        Me.btnCancelar.BackColor = System.Drawing.Color.DeepSkyBlue
-        Me.btnCancelar.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.btnCancelar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CadetBlue
-        Me.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnCancelar.Location = New System.Drawing.Point(144, 187)
-        Me.btnCancelar.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.btnCancelar.Name = "btnCancelar"
-        Me.btnCancelar.Size = New System.Drawing.Size(92, 32)
-        Me.btnCancelar.TabIndex = 7
-        Me.btnCancelar.Text = "Cancelar"
-        Me.btnCancelar.UseVisualStyleBackColor = False
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.BackColor = System.Drawing.Color.White
-        Me.Label3.Location = New System.Drawing.Point(65, 47)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(46, 25)
-        Me.Label3.TabIndex = 10
-        Me.Label3.Text = "LOGIN"
-        '
-        'ErrorValidacion
-        '
-        Me.ErrorValidacion.ContainerControl = Me
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox1.Image = Global.Clase_III_Parcial.My.Resources.Resources.tumblr_619bffaeff48dcaec1f90141532ea4ac_7ee4dae5_500
-        Me.PictureBox1.Location = New System.Drawing.Point(0, 38)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(495, 244)
-        Me.PictureBox1.TabIndex = 4
-        Me.PictureBox1.TabStop = False
+        Me.ErrorProvider1.ContainerControl = Me
         '
         'Login
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 25.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(493, 279)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Panel2)
-        Me.Controls.Add(Me.Panel1)
-        Me.Controls.Add(Me.btnIngresar)
-        Me.Controls.Add(Me.btnCancelar)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.ClientSize = New System.Drawing.Size(505, 325)
         Me.Controls.Add(Me.PanelSuperior)
-        Me.Font = New System.Drawing.Font("Bebas Neue", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.btnIniciar)
+        Me.Controls.Add(Me.btnMostrarContra)
+        Me.Controls.Add(Me.txtContrasena)
+        Me.Controls.Add(Me.lblOlvideContra)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Controls.Add(Me.btnOcultarContra)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Login"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Login"
         Me.PanelSuperior.ResumeLayout(False)
-        Me.Panel2.ResumeLayout(False)
-        Me.Panel2.PerformLayout()
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
-        CType(Me.ErrorValidacion, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -238,16 +192,13 @@ Partial Class Login
     Friend WithEvents PanelSuperior As Panel
     Friend WithEvents btnMinimizar As Button
     Friend WithEvents btnCerrar As Button
-    Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Panel2 As Panel
-    Friend WithEvents txtContraseña As TextBox
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents btnIniciar As Button
+    Friend WithEvents btnMostrarContra As Button
+    Friend WithEvents txtContrasena As TextBox
+    Friend WithEvents lblOlvideContra As LinkLabel
     Friend WithEvents txtUsuario As TextBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents btnIngresar As Button
-    Friend WithEvents btnCancelar As Button
-    Friend WithEvents Label3 As Label
-    Friend WithEvents ToolTip As ToolTip
-    Friend WithEvents ErrorValidacion As ErrorProvider
+    Friend WithEvents btnOcultarContra As Button
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents ToolTip1 As ToolTip
 End Class
--- Clase III Parcial/Clase III Parcial/Proyecto/Login.vb
+++ Clase III Parcial/Clase III Parcial/Proyecto/Login.vb
@@ -1,79 +1,78 @@
-﻿Imports System.Runtime.InteropServices
+﻿Imports System.ComponentModel
+
 Public Class Login
-    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
-    Private Shared Sub ReleaseCapture()
-    End Sub
 
-    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
-    Private Shared Sub SendMessage(ByVal hWnd As System.IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer)
-    End Sub
-
-    Private Sub PanelSuperior_MouseMove(sender As Object, e As MouseEventArgs) Handles PanelSuperior.MouseMove
-        ReleaseCapture()
-        SendMessage(Me.Handle, &H112&, &HF012&, 0)
-    End Sub
-
-    Dim Usuario As String
-    Dim contra As String
+    Dim conexion As conexion = New conexion
 
     Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Usuario = "ADMIN"
-        contra = "12345"
+        conexion.conectar()
+        btnOcultarContra.Visible = False
+        txtContrasena.PasswordChar = "●"
     End Sub
 
-    Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+    Private Sub btnIniciar_Click(sender As Object, e As EventArgs) Handles btnIniciar.Click
+        Dim usuario, contrasena As String
+        usuario = txtUsuario.Text
+        contrasena = txtContrasena.Text
 
-        If txtContraseña.Text = contra And txtUsuario.Text = Usuario Then
-            MsgBox("Ingreso al sistema Exitosamente!", vbInformation + vbOK, "Informacion")
-            Me.Hide()
-            ChargeScreen.Show()
+        ' If conexion.inicioSesion(usuario, contrasena) = True Then
+        'ChargeScreen.Show()
+        'Me.Hide()
+        ' Else
+        'MessageBox.Show("Usuario o Contraseña incorrectos!", "Error de Ingreso", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        ' End If
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
+        recuperar.Show()
+    End Sub
+
+    Private Sub btnMostrarContra_Click(sender As Object, e As EventArgs) Handles btnMostrarContra.Click
+        btnMostrarContra.Visible = False
+        btnOcultarContra.Visible = True
+        txtContrasena.PasswordChar = ""
+    End Sub
+
+    Private Sub btnOcultarContra_Click(sender As Object, e As EventArgs) Handles btnOcultarContra.Click
+        btnOcultarContra.Visible = False
+        btnMostrarContra.Visible = True
+        txtContrasena.PasswordChar = "●"
+    End Sub
+
+    Private Sub txtUsuario_Validating(sender As Object, e As CancelEventArgs) Handles txtUsuario.Validating
+        If DirectCast(sender, TextBox).Text.Length > 0 Then
+            Me.ErrorProvider1.SetError(sender, "")
         Else
-            MsgBox("Ingreso fallido, vuelva a intentarlo...", vbObjectError + vbOK, "Error")
-            txtContraseña.Clear()
-            txtUsuario.Clear()
+            Me.ErrorProvider1.SetError(sender, "Es un campo obligatorio")
         End If
     End Sub
 
-    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
-        If MsgBox("Desea salir de la aplicacion?", vbQuestion + vbYesNo, "Salir") = vbYes Then
-            End
+    Private Sub txtContrasena_Validating(sender As Object, e As CancelEventArgs) Handles txtContrasena.Validating
+        If DirectCast(sender, TextBox).Text.Length > 0 Then
+            Me.ErrorProvider1.SetError(sender, "")
+        Else
+            Me.ErrorProvider1.SetError(sender, "Es un campo obligatorio")
         End If
-    End Sub
-
-    Private Sub btnMinimizar_Click(sender As Object, e As EventArgs) Handles btnMinimizar.Click
-        Me.WindowState = FormWindowState.Minimized
-    End Sub
-
-    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
-        End
     End Sub
 
     Private Sub txtUsuario_MouseHover(sender As Object, e As EventArgs) Handles txtUsuario.MouseHover
-        ToolTip.SetToolTip(txtUsuario, "Ejemplo: ADMIN")
-        ToolTip.ToolTipTitle = "Usuario"
-        ToolTip.ToolTipIcon = ToolTipIcon.Info
+        ToolTip1.SetToolTip(txtUsuario, "Introduzca el numero de identidad")
+        ToolTip1.ToolTipTitle = "Ingrese Valores"
+        ToolTip1.ToolTipIcon = ToolTipIcon.Info
     End Sub
 
-    Private Sub txtContraseña_MouseHover(sender As Object, e As EventArgs) Handles txtContraseña.MouseHover
-        ToolTip.SetToolTip(txtContraseña, "Ejemplo: 12345")
-        ToolTip.ToolTipTitle = "Contraseña"
-        ToolTip.ToolTipIcon = ToolTipIcon.Info
+    Private Sub txtContrasena_MouseHover(sender As Object, e As EventArgs) Handles txtContrasena.MouseHover
+        ToolTip1.SetToolTip(txtContrasena, "Introduzca su contrasña")
+        ToolTip1.ToolTipTitle = "Ingrese Valores"
+        ToolTip1.ToolTipIcon = ToolTipIcon.Info
     End Sub
 
-    Private Sub txtUsuario_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtUsuario.Validating
-        If DirectCast(sender, TextBox).Text.Length > 0 Then
-            Me.ErrorValidacion.SetError(sender, "")
-        Else
-            Me.ErrorValidacion.SetError(sender, "Es un campo obligatorio")
-        End If
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lblOlvideContra.LinkClicked
+        recuperar.Show()
+
     End Sub
 
-    Private Sub txtContraseña_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtContraseña.Validating
-        If DirectCast(sender, TextBox).Text.Length > 0 Then
-            Me.ErrorValidacion.SetError(sender, "")
-        Else
-            Me.ErrorValidacion.SetError(sender, "Es un campo obligatorio")
-        End If
-    End Sub
 
 End Class
